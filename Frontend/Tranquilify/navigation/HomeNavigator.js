03c6c5c838ed27a4739f41a5e62a07f1a88d59ad
zmarabeas <zmarabeas@gmail.com>
2025-02-10 14:41:32 -0500

    add navigation and authentication template

---
+++ Frontend/Tranquilify/navigation/HomeNavigator.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MoodTrackerScreen from '../screens/MoodTrackerScreen.js';
+import BreathingExerciseScreen from '../screens/BreathingExerciseScreen.js';
+import JournalScreen from '../screens/JournalScreen.js';
+
+const HomeTab = createBottomTabNavigator();
+
+export default function HomeNavigator(props) {
+  return (
+    <HomeTab.Navigator>
+      <HomeTab.Screen
+        name="MoodTrackerScreen"
+        component={MoodTrackerScreen}
+        options={{ title: 'Home' }}
+      />
+      <HomeTab.Screen
+        name="Breathing"
+        component={BreathingExerciseScreen}
+        options={{ title: 'Breathing' }}
+      />
+      <HomeTab.Screen
+        name="Journal"
+        component={JournalScreen}
+        options={{ title: 'Journal' }}
+        userToken={props.userToken}
+      />
+    </HomeTab.Navigator>
+  );
+}
