7f0b784d50ab652d2e4ffda2adfacdc964eb4f5f
zmarabeas <zmarabeas@gmail.com>
2025-04-07 15:35:22 -0400

    remove test file

--- Frontend/Tranquilify/jest.setup.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler/jestSetup';
-import '@testing-library/jest-native/extend-expect';
-
-// Mock Expo's Constants
-jest.mock('expo-constants', () => ({
-  Constants: { manifest: { extra: { apiUrl: 'https://example.com/api' } } }
-}));
