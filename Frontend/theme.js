3827afcd56b97c99e5636eae7fb0bbb515cbb34d
Hiba Rababeh <hj0207@wayne.edu>
2025-04-08 18:01:39 -0400

    added theme.js to the frontend

---
+++ Frontend/theme.js
@@ -0,0 +1,23 @@
+export default {
+    primary: '#1e90ff',    
+    secondary: '#ff6347',  
+    accent: '#00adf5',    
+    text: '#333333',
+    textLight: '#888888',
+    background: '#f5f5f5', 
+    white: '#ffffff',
+    spacing: {
+      xs: 4,
+      sm: 8,
+      md: 16,
+      lg: 24,
+    },
+    fontSize: {
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 24,
+    },
+    // etc.
+  };
+  
