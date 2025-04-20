03d06b10ac63bdeb32f647abab0ebfb53f80db97
zmarabeas <zmarabeas@gmail.com>
2025-04-20 16:43:48 -0400

    update app test

--- Backend/src/test/java/Tranquilify/demo/TranquilifyApplicationTests.java
+++ Backend/src/test/java/Tranquilify/demo/TranquilifyApplicationTests.java
@@ -1,6 +1,7 @@
 package Tranquilify.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
